f111f059f60b4eea0a4fd33d814eb6601db4d5e3
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-11-04 20:09:31 -0400

    update controller path

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main.js');
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
