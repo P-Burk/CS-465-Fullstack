cca8b38e829c67d67b9bc64bb7393b86ef138f66
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-11-13 00:50:51 -0500

    updated rooms controller to use JSON data

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var roomInfo = JSON.parse(fs.readFileSync('./data/roomInfo.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', {title: 'Travlr Getaways', activeTabRooms: true });
+    res.render('rooms', {title: 'Travlr Getaways', activeTabRooms: true, roomInfo });
 };
 module.exports = {
     rooms
