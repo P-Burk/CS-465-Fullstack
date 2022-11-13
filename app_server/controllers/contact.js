efd17f365460cfd7766f05da1d64ebfab91e3d5e
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-11-13 01:48:20 -0500

    updated contact controller to use JSON data

--- app_server/controllers/contact.js
+++ app_server/controllers/contact.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+var contactInfo = JSON.parse(fs.readFileSync('./data/contactInfo.json', 'utf8'));
+console.log('contactInfo: ', contactInfo);
+
 /* GET contact view */
 const contact = (req, res) => {
-    res.render('contact', {title: 'Travlr Getaways', activeTabContact: true });
+    res.render('contact', {title: 'Travlr Getaways', activeTabContact: true, contactInfo });
 };
 module.exports = {
     contact
