1d04d6a513512a598df2a4cb7ee222cf6ecea3c0
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-12-03 02:35:53 -0500

    updated trip-data service

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Trip } from '../models/trip';
+
+@Injectable()
 export class TripDataService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  private apiBaseUrl = 'http://localhost:3000/api/';
+
+  public getTrips(): Promise<Trip[]> {
+    console.log('Inside TripDataService#getTrips');
+    return this.http
+      .get(`${this.apiBaseUrl}trips`)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError)
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Something has gone wrong: ', error);
+    return Promise.reject(error.message || error);
+  } 
 }
