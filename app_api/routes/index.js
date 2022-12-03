78d6785d19e7d7bc998383f598cc47c09e443727
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-12-03 03:35:22 -0500

    fixed index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,7 +5,7 @@ const tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
     .post(tripsController.tripsAddTrip);
 
 router
