fe4960d22a45bb33b7a8c380d6fd94c142f1c2ff
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-12-03 22:38:40 -0500

    added functionality to get trip code and update

--- app_admin/src/app/edit-trip/edit-trip.component.ts
+++ app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { TripDataService } from '../services/trip-data.service';
 
 @Component({
   selector: 'app-edit-trip',
   templateUrl: './edit-trip.component.html',
   styleUrls: ['./edit-trip.component.css']
 })
+
 export class EditTripComponent implements OnInit {
 
-  constructor() { }
+  submitted: boolean = false;
+  editForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private tripService: TripDataService
+  ) { }
 
   ngOnInit() {
+    // retrieve stashed tripId
+    let tripCode = localStorage.getItem("tripCode");
+    if (!tripCode) {
+      alert("Something wrong, couldn't find where I stashed tripCode!");
+      this.router.navigate(['']);
+      return;
+    }
+
+    //initialize form
+    this.editForm = this.formBuilder.group({
+      _id: [],
+      code: [tripCode, Validators.required],
+      name: ["", Validators.required],
+      length: ["", Validators.required],
+      start: ["", Validators.required],
+      resort: ["", Validators.required],
+      perPerson: ["", Validators.required],
+      image: ["", Validators.required],
+      description: ["", Validators.required]
+    })
+
+    this.tripService.getTrip(tripCode)
+      .then(data => {
+        console.log(data);
+        this.editForm.patchValue(data);
+      })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.editForm.valid) {
+      this.tripService.updateTrip(this.editForm.value)
+        .then(data => {
+          console.log(data);
+          this.router.navigate(['']);
+        });
+    }
   }
 
 }
