7d115126ab8702c9e5d7b1ed59b7a2be56a54d4c
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-12-03 20:57:36 -0500

    added getTrip() and updateTrip()

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -9,20 +9,42 @@ export class TripDataService {
   constructor(private http: Http) { }
 
   private apiBaseUrl = 'http://localhost:3000/api/';
+  private tripURL = `${this.apiBaseUrl}trips/`
 
+  //get multiple trips
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataService#getTrips');
     return this.http
-      .get(`${this.apiBaseUrl}trips`)
+      .get(this.tripURL)
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError)
   }
 
+  //get single trip
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log('Inside TripDataService#getTrip(tripCode)');
+    return this.http
+      .get(this.tripURL + tripCode)
+      .toPromise()
+      .then(response => response.json() as Trip)
+      .catch(this.handleError);
+  }
+
   public addTrip(formData: Trip): Promise<Trip> {
     console.log('Inside TripDataService#addTrip');
     return this.http
-      .post(`${this.apiBaseUrl}trips`, formData)
+      .post(this.tripURL, formData)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  public updateTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#updateTrip');
+    console.log(formData);
+    return this.http
+      .put(this.tripURL + formData.code, formData)
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
